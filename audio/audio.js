2db663249c852c4862220f84fef28c1f3fc2b191
Kevin Ho <ho.kevins@gmail.com>
2017-11-20 18:02:10 -0500

    EOD 2017-11-20

--- audio/audio.js
+++ audio/audio.js
@@ -36,3 +36,118 @@ sound.on('ended', function(seconds) {
 });
 
 
+// Any copyright is dedicated to the Public Domain.
+// http://creativecommons.org/publicdomain/zero/1.0/
+
+/*********************************************
+This servo module demo turns the servo around
+1/10 of its full rotation  every 500ms, then
+resets it after 10 turns, reading out position
+to the console at each movement.
+*********************************************/
+
+var tessel = require('tessel');
+var servolib = require('servo-pca9685');
+
+var servo = servolib.use(tessel.port['A']);
+
+var servo1 = 1; // We have a servo plugged in at position 1
+
+servo.on('ready', function () {
+  var position = 0;  //  Target position of the servo between 0 (min) and 1 (max).
+
+  //  Set the minimum and maximum duty cycle for servo 1.
+  //  If the servo doesn't move to its full extent or stalls out
+  //  and gets hot, try tuning these values (0.05 and 0.12).
+  //  Moving them towards each other = less movement range
+  //  Moving them apart = more range, more likely to stall and burn out
+  servo.configure(servo1, 0.05, 0.12, function () {
+    setInterval(function () {
+      console.log('Position (in range 0-1):', position);
+      //  Set servo #1 to position pos.
+      servo.move(servo1, position);
+
+      // Increment by 10% (~18 deg for a normal servo)
+      position += 0.1;
+      if (position > 1) {
+        position = 0; // Reset servo position
+      }
+    }, 500); // Every 500 milliseconds
+  });
+});
+
+/////
+
+
+// $(document).ready(function() {
+//   var timer;
+// document.getElementById('timer').innerHTML =
+// 25 + ":" + 00;
+// startTimer();
+
+// function startTimer() {
+
+//   var presentTime = document.getElementById('timer').innerHTML;
+//   var timeArray = presentTime.split(/[:]+/);
+//   var m = timeArray[0];
+//   var s = checkSecond((timeArray[1] - 1));
+//   if(s==59){m=m-1}
+//   //if(m<0){alert('timer completed')}
+
+//   document.getElementById('timer').innerHTML =
+//   m + ":" + s;
+//   timer = setTimeout(startTimer, 1000);
+// }
+
+// function checkSecond(sec) {
+//   if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+//   if (sec < 0) {sec = "59"};
+//   return sec;
+// }
+
+// $("#party_time").on('click', function(){
+//   console.log("The party button was clicked.");
+
+// sound.play();
+
+// // Party Time:  plays song continuously
+// sound.on('ended', function(seconds) {
+//   sound.play();
+// });
+
+// servo.on('ready', function () {
+//   var position = 0;  //  Target position of the servo between 0 (min) and 1 (max).
+
+//   //  Set the minimum and maximum duty cycle for servo 1.
+//   //  If the servo doesn't move to its full extent or stalls out
+//   //  and gets hot, try tuning these values (0.05 and 0.12).
+//   //  Moving them towards each other = less movement range
+//   //  Moving them apart = more range, more likely to stall and burn out
+//   servo.configure(servo1, 0.05, 0.12, function () {
+//     setInterval(function () {
+//       console.log('Position (in range 0-1):', position);
+//       //  Set servo #1 to position pos.
+//       servo.move(servo1, position);
+
+//       // Increment by 10% (~18 deg for a normal servo)
+//       position += 0.1;
+//       if (position > 1) {
+//         position = 0; // Reset servo position
+//       }
+//     }, 500); // Every 500 milliseconds
+//   });
+// });
+
+// });
+// $("#stop").on('click', function(){
+//   console.log("The stop button was clicked.");
+//   clearTimeout(timer);
+// });
+
+// $("#reset").on('click', function(){
+//   console.log("The reset button was clicked.");
+//   window.location.reload();
+// });
+
+// });
+
--- index.js
+++ index.js
@@ -1,42 +1,52 @@
+'use strict';
+
+// Import the interface to Tessel hardware
 var tessel = require('tessel');
 var http = require('http');
-var express = require('express');
-var router = express.Router();
-var views = require('./views');
-var audio = require('../audio/audio.js');
+var fs = require('fs');
+var url = require('url');
 
-//audio.on => music on
-//audio.off => music off
-//audio.partyBreak => music for 5 min
+var server = http.createServer(function (request, response) {
+  var urlParts = url.parse(request.url, true);
+  var ledRegex = /leds/;
 
-var server = http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello from Tessel!\n");
-})
-    
+  if (urlParts.pathname.match(ledRegex)) {
+    toggleLED(urlParts.pathname, request, response);
+  } else {
+    showIndex(urlParts.pathname, request, response);
+  }
+});
 
 server.listen(8080);
-console.log("Server running http://192.168.1.101");
 
-router.get('/', (req, res, next) => {
-    //listen for the end of the timer
-})
+console.log('Server running at http://192.168.1.101:8080/');
 
-router.get('/party', (req, res, next) => {
-    //set timeout 
-    //run song for 5 min
-})
+function showIndex (url, request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  fs.readFile(__dirname + '/audio/index.html', function (err, content) {
+    if (err) {
+      throw err;
+    }
 
-//event handlers ('on click')
-//party time
-    //set timeout - 5min
-    //stop countdown
-        //countdown = 0
-    //start music
+    response.end(content);
+  });
+}
 
-//stop 
-    //stops music
+function toggleLED (url, request, response) {
+  var indexRegex = /(\d)$/;
+  var result = indexRegex.exec(url);
+  var index = +result[1];
 
-//reset
-    //resume countdown
+  var led = tessel.led[index];
 
+  led.toggle(function (err) {
+    if (err) {
+      console.log(err);
+      response.writeHead(500, {"Content-Type": "application/json"});
+      response.end(JSON.stringify({error: err}));
+    } else {
+      response.writeHead(200, {"Content-Type": "application/json"});
+      response.end(JSON.stringify({on: led.isOn}));
+    }
+  });
+}
